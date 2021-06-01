482c2e414ccbdcd84ddea74f8f0931caa8959ea2
Alfonso Domínguez Angarita <aldoan87@gmail.com>
2021-06-01 11:10:11 -0500

    Adding KXmlParse manually in the project

--- library/src/main/java/com/github/siyamed/shapeimageview/path/parser/SvgToPath.java
+++ library/src/main/java/com/github/siyamed/shapeimageview/path/parser/SvgToPath.java
@@ -5,7 +5,6 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.Log;
 
-import org.kxml2.io.KXmlParser;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
